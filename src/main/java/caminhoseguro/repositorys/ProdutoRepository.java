2a89c97e51a7889658e547953fa9ee3b82612515
Seidimussa <sedja2015@gmail.com>
2025-08-20 22:13:06 +0000

    src\main\java\caminhoseguro\repositorys\ProdutoRepository.java

--- src/main/java/caminhoseguro/repositorys/ProdutoRepository.java
+++ src/main/java/caminhoseguro/repositorys/ProdutoRepository.java
@@ -1,5 +1,12 @@
 package caminhoseguro.repositorys;
 
-public interface ProdutoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import caminhoseguro.entitys.Produto;
+
+//Repositorio que herda metodos prontos como findAll, save, deleteById
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    
 
 }
